b639e1e2ef3af78d46d42035a17810915637b7fc
Arman Nihal <armansaliknihal@gmail.com>
2025-09-19 15:18:25 +0300

    initialize default project

---
+++ TDDExperiment/TDDExperiment/Item.swift
@@ -0,0 +1,18 @@
+//
+//  Item.swift
+//  TDDExperiment
+//
+//  Created by Arman Nihal on 19.9.2025.
+//
+
+import Foundation
+import SwiftData
+
+@Model
+final class Item {
+    var timestamp: Date
+    
+    init(timestamp: Date) {
+        self.timestamp = timestamp
+    }
+}
---
+++ TDDExperiment/TDDExperimentTests/TDDExperimentTests.swift
@@ -0,0 +1,17 @@
+//
+//  TDDExperimentTests.swift
+//  TDDExperimentTests
+//
+//  Created by Arman Nihal on 19.9.2025.
+//
+
+import Testing
+@testable import TDDExperiment
+
+struct TDDExperimentTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
